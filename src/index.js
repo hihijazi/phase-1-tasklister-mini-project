a39e60add052ff9f157bc2a588c9c58128f13c7d
hadil <hihijazi@gmail.com>
2023-11-16 16:49:31 -0500

    save changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// document.addEventListener("DOMContentLoaded", () => {
+//   //
+// });
+
+// const main = document.querySelection("main-content")
+// console.log(main)
+
+const taskForm = document.querySelector("#create-task-form");
+const resultElement = document.querySelector("#result");
+taskForm.addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the form from submitting and refreshing the page
+  const result = "hiking";
+  resultElement.textContent = result;
 });
+
+// function documentquerySelection() {
+//   return hiking();
+// }
